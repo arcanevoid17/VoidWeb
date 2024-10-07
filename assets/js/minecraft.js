c4ba0fe3db462283cc11f57b8c90ad7face8bd4a
arcanevoid17 <goddvoid17@gmail.com>
2024-10-07 17:40:41 +0800

    ss

--- assets/js/minecraft.js
+++ assets/js/minecraft.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentLocation = savedLocation;
 
     const exchangeRates = {
-        'USD': 56.43,
+        'USD': 56.86,
         'EUR': 1,
 
     };
